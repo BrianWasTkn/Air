85ddb21460a06a3ebc6c8f1ef5900cfae3e412dc
Crazyshooting <danielstarwalker2000@gmail.com>
2020-10-26 23:06:16 +0300

    Fixed the vote bug?

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async bot => {
     bot.user.setActivity('air help', { type: 'PLAYING' });
     console.log(`${bot.user.tag} is online. Servers: ${bot.guilds.cache.size}, Users: ${bot.users.cache.size}`);
-    await bot.dbl.postStats(bot.guilds.cache.size);
+    /*await bot.dbl.postStats(bot.guilds.cache.size);*/
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ const { Collection } = require('discord.js');
 const MongoClient = require('./utils/MongoClient');
 const DBL = require("dblapi.js");
 const bot = new MongoClient();
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1MjA2MTMyNDY4MjQ2MTI1NSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjAzNzM1OTUzfQ.p4A4LAJ3BF-BxGBU_OYvpf7gg2e2w8ja9yuu_eDtahk', bot);
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1MjA2MTMyNDY4MjQ2MTI1NSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjAzNzM1OTUzfQ.p4A4LAJ3BF-BxGBU_OYvpf7gg2e2w8ja9yuu_eDtahk', { webhookAuth: '69420gamersdashcruftgavcs', webhookServer: server }, bot);
 
 bot.login(process.env.TOKEN);
 
@@ -15,8 +21,10 @@ bot.dbl = dbl;
 require('./utils/handlers/command')(bot);
 require('./utils/handlers/event')(bot);
 
-dbl.webhook.on('vote', async (bot, vote) => {
+dbl.webhook.on('vote', async (vote) => {
     const user = await bot.fetchUser(vote.user);
+
+    console.log(user.coinsInWallet);
 
     user.coinsInWallet += 4000;
     await user.save();
